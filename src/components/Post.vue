efd69f6debf83277e90a99ae19ed3dee995ef90b
t-niiru <t.niiru@gmail.com>
2018-09-26 16:51:15 +0900

    :sparkles: post message add url link

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,16 +3,17 @@
     <div class="media-content">
       <div class="content">
         <img :src="require('../assets/images/' + charNo + '.gif')">
-        <p>
-          <strong>{{ name }} があらわれた</strong>
-          <span v-html="formattedPost" />
-        </p>
+        <div class="user-name"><strong>Name:{{ name }}</strong></div>
+        <div class="massege-body"><span v-html="formattedPost" /></div>
       </div>
     </div>
   </li>
 </template>
 
 <script>
+import h from 'htmlspecialchars'
+import { link } from 'autolinker'
+
 export default {
   props: [
     'name',
@@ -23,7 +24,8 @@ export default {
   ],
   computed: {
     formattedPost () {
-      return this.body
+      // return this.body
+      return link(h(this.body))
     }
   }
 }
